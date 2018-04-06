e1ecc963f226ac47ca85754ad6e472c5f9a089f8
Justin <jdeanwaite@gmail.com>
2018-04-06 16:17:38 -0700

    Removed extra horizontal padding from the header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -36,40 +36,38 @@ class Header extends React.Component {
     return (
       <div>
         <Navbar className="page-navbar" color="primary" dark expand="md">
-          <div className="container-fluid">
-            <Link className="navbar-brand" to="/" onClick={this.close}>
-              <div style={{ display: 'flex', flexFlow: 'row nowrap', alignItems: 'center' }}>
-                <img className="site-logo" src={logo} alt="JW" />
-                <div style={{ display: 'flex', flexFlow: 'column nowrap' }}>
-                  <span>Justin Waite</span>
-                  <span style={{ fontSize: 14 }}>Web and Mobile Developer</span>
-                </div>
+          <Link className="navbar-brand" to="/" onClick={this.close}>
+            <div style={{ display: 'flex', flexFlow: 'row nowrap', alignItems: 'center' }}>
+              <img className="site-logo" src={logo} alt="JW" />
+              <div style={{ display: 'flex', flexFlow: 'column nowrap' }}>
+                <span>Justin Waite</span>
+                <span style={{ fontSize: 14 }}>Web and Mobile Developer</span>
               </div>
-            </Link>
-            <NavbarToggler onClick={this.toggle} />
-            <Collapse isOpen={this.state.isOpen} navbar>
-              <Nav className="ml-auto" navbar>
-                <NavItem>
-                  <Link className="nav-link" to="/showcase" onClick={this.close}>
+            </div>
+          </Link>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <Link className="nav-link" to="/showcase" onClick={this.close}>
                     Showcase
-                  </Link>
-                </NavItem>
-                <NavItem>
-                  <NavLink target="_blank" href="https://github.com/jdeanwaite">Github</NavLink>
-                </NavItem>
-                <NavItem>
-                  <Link className="nav-link" to="/lab" onClick={this.close}>
+                </Link>
+              </NavItem>
+              <NavItem>
+                <NavLink target="_blank" href="https://github.com/jdeanwaite">Github</NavLink>
+              </NavItem>
+              <NavItem>
+                <Link className="nav-link" to="/lab" onClick={this.close}>
                     Lab
-                  </Link>
-                </NavItem>
-                <NavItem>
-                  <Link className="nav-link" to="/hire-me" onClick={this.close}>
+                </Link>
+              </NavItem>
+              <NavItem>
+                <Link className="nav-link" to="/hire-me" onClick={this.close}>
                     Hire Me
-                  </Link>
-                </NavItem>
-              </Nav>
-            </Collapse>
-          </div>
+                </Link>
+              </NavItem>
+            </Nav>
+          </Collapse>
         </Navbar>
       </div>
     );
